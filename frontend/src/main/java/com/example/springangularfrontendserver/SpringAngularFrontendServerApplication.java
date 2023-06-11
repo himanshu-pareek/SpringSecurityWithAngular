2cddd00eba2c53b87f823dca96e7fcb86a97febc
Himanshu Pareek <himanshupareekiit01@gmail.com>
2023-06-11 12:20:56 +0530

    Initializes Spring + Angular app with basic spring security

---
+++ frontend/src/main/java/com/example/springangularfrontendserver/SpringAngularFrontendServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springangularfrontendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularFrontendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularFrontendServerApplication.class, args);
+	}
+
+}
---
+++ frontend/src/test/java/com/example/springangularfrontendserver/SpringAngularFrontendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springangularfrontendserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularFrontendServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
