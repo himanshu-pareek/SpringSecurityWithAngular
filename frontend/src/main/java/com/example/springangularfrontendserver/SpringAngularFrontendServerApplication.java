a25015181f396b0f462d2f11ba55e15f28b36f8b
Himanshu Pareek <himanshupareekiit01@gmail.com>
2023-06-15 18:04:11 +0530

    Puts SecurityConfiguration in it's own file

--- frontend/src/main/java/com/example/springangularfrontendserver/SpringAngularFrontendServerApplication.java
+++ frontend/src/main/java/com/example/springangularfrontendserver/SpringAngularFrontendServerApplication.java
@@ -2,29 +2,11 @@ package com.example.springangularfrontendserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class SpringAngularFrontendServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAngularFrontendServerApplication.class, args);
-	}
-
-    @Configuration
-    protected static class SecurityConfiguration {
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-            )
-                .httpBasic(Customizer.withDefaults());
-            return http.build();
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularFrontendServerApplication.class, args);
     }
-
 }
