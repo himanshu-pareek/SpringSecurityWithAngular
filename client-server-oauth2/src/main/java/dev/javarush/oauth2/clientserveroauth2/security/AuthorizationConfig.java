378da09c94c821e0f85a6802752122979541d2de
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-01-06 18:13:16 +0530

    Logout from Authorization Server as well.

--- client-server-oauth2/src/main/java/dev/javarush/oauth2/clientserveroauth2/security/AuthorizationConfig.java
+++ client-server-oauth2/src/main/java/dev/javarush/oauth2/clientserveroauth2/security/AuthorizationConfig.java
@@ -1,10 +1,15 @@
 package dev.javarush.oauth2.clientserveroauth2.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestAttributeHandler;
@@ -12,7 +17,22 @@ import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 @Configuration
+@EnableWebFluxSecurity
 public class AuthorizationConfig {
+
+    @Autowired
+    private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+            new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
+
+        // Sets the location that the End-User's User Agent will be redirected to
+        // after the logout has been performed at the Provider
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+
+        return oidcLogoutSuccessHandler;
+    }
 
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
@@ -26,6 +46,9 @@ public class AuthorizationConfig {
                 exchange -> exchange
                         .anyExchange().authenticated()
         );
+        http.logout(logoutSpec -> {
+            logoutSpec.logoutSuccessHandler(oidcLogoutSuccessHandler());
+        });
         return http.build();
     }
 
